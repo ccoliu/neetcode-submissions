08a128a7f5f03db8ab05d689113eb12b3065692a
ccoliu <52516787+ccoliu@users.noreply.github.com>
2026-06-18 15:25:56 +0800

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0, r = 0;
+        int n = prices.size();
+        int ans = 0;
+        while(r < n) {
+            if (l == r) {
+                r++;
+                continue;
+            }
+            else {
+                if (prices[r] < prices[l]) {
+                    l = r;
+                }
+                else {
+                    ans = max(ans, prices[r] - prices[l]);
+                    r++;
+                }
+            }
+        }
+        return ans;
+    }
+};
