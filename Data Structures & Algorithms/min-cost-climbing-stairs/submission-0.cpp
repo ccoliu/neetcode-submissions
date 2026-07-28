79acbe91a4a47a91d58e88f751d0075363861171
ccoliu <52516787+ccoliu@users.noreply.github.com>
2026-07-28 14:52:22 +0800

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        cost.push_back(0);
+        int n = cost.size();
+        vector<int> dp(n + 1, 0);
+
+        dp[1] = cost[0];
+
+        for(int i = 2; i <= n;i++) {
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i-1];
+        }
+
+        return dp[n];
+    }
+};
