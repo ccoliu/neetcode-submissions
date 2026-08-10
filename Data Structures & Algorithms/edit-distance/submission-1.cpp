c8c34dc7802f0201b0fd1ff32d0cadeb7c79709c
ccoliu <52516787+ccoliu@users.noreply.github.com>
2026-08-10 15:44:02 +0800

    Add: edit-distance - submission-1

---
+++ Data Structures & Algorithms/edit-distance/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int dfs(string& word1, string& word2, int idx1, int idx2) {
+        if (idx1 >= word1.length() && idx2 >= word2.length()) return 0;
+
+        if (idx1 >= word1.length() && idx2 < word2.length()) return 1 + dfs(word1, word2, idx1, idx2 + 1);
+
+        if (idx2 >= word2.length() && idx1 < word1.length()) return 1 + dfs(word1, word2, idx1 + 1, idx2);
+
+        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
+
+        if (word1[idx1] == word2[idx2]) {
+            return dp[idx1][idx2] = dfs(word1, word2, idx1 + 1, idx2 + 1);
+        } 
+        else {
+            return dp[idx1][idx2] = 1 + min({dfs(word1, word2, idx1, idx2 + 1), dfs(word1, word2, idx1 + 1, idx2 + 1), dfs(word1, word2, idx1 + 1, idx2)});
+        }
+    }
+
+    int minDistance(string word1, string word2) {
+        int w1 = word1.length(), w2 = word2.length();
+        dp.assign(w1 + 1, vector<int>(w2 + 1, -1));
+        return dfs(word1, word2, 0, 0);
+    }
+};
